9f7e61970e51f297b61c8b331b5487db88b41460
nicollas.lopes <nicollaslopesn@gmail.com>
2026-01-09 07:24:26 -0300

    feat: creating method to validate flags

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -16,6 +17,8 @@ var userPtr *string
 var UserPtr *string
 var passwordPtr *string
 var PasswordPtr *string
+var paramsPtr *string
+var valuesPtr *string
 
 func main() {
 
@@ -24,20 +27,45 @@ func main() {
 	UserPtr = flag.String("U", "", "user wordlist\n")
 	passwordPtr = flag.String("p", "", "password\n")
 	PasswordPtr = flag.String("P", "", "password wordlist\n")
+	paramsPtr = flag.String("params", "", "params\n")
+	valuesPtr = flag.String("values", "", "values\n")
 	flag.Parse()
+	validateFlags()
 
 	params := make(map[string]string)
 
 	params["url"] = "http://localhost:9000/teste.php"
 
+	//  -params "username,password,page" -values "^USER^,^PASS^,test" -msg "incorrect"
+	// user=^USER^&password=^PASS^&submit=Login:Login Failed
 	makeRequest(params)
 }
 
 func makeRequest(params map[string]string) {
 
+	var user string
+
+	paramsTotal := len(strings.Split(*paramsPtr, ","))
+	valuesTotal := len(strings.Split(*valuesPtr, ","))
+
+	if paramsTotal != valuesTotal {
+		fmt.Println("Params does not match with values!")
+		os.Exit(1)
+	}
+
+	if *userPtr != "" {
+		user = *userPtr
+	}
+
+	if *UserPtr != "" {
+		user = "test"
+	}
+
+	userParam := strings.Split(*paramsPtr, ",")
+
 	data := url.Values{}
-	data.Set("username", "admin")
-	data.Set("password", "123")
+	data.Set(userParam[0], user)
+	data.Set(userParam[1], "123")
 
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", params["url"], strings.NewReader(data.Encode()))
@@ -68,4 +96,16 @@ func makeRequest(params map[string]string) {
 		fmt.Println("Found!")
 	}
 
+}
+
+func validateFlags() {
+	if *userPtr != "" && *UserPtr != "" {
+		fmt.Println("You can not use -u and -U flags together")
+		os.Exit(1)
+	}
+
+	if *passwordPtr != "" && *PasswordPtr != "" {
+		fmt.Println("You can not use -p and -P flags together")
+		os.Exit(1)
+	}
 }
